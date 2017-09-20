c854eef2385814c72ff9197eeef3c85a98a049cd
Blockchain Forest <31790462+blockchainforest@users.noreply.github.com>
2017-09-21 01:39:03 +0800

    Update TREE.sol

--- TREE.sol
+++ TREE.sol
@@ -6,7 +6,7 @@ Blockchain DAPP . Decentralized Token Private Placement Programme (DTPPP) . Envi
 Fintech Facilitation Office:
 CoPlace 1, 2270 Jalan Usahawan 2, Cyber 6, 63000 Cyberjaya. West Malaysia
 Support Line: +603.9212.6666
-dapp@blockchainforest.io
+support@treetoken.io
 
 Malaysia . Hong Kong . Amsterdam . UK.  China
 
